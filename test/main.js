3b29287c22e30205a7196a0bca22227ca7f6dc48
piglovesyou <thepiglovesyou@gmail.com>
2018-02-11 00:29:58 +0900

    refac: wip: more tests

--- test/main.js
+++ test/main.js
@@ -1,27 +1,101 @@
 // @flow
 
 import assert from 'assert';
+import stream from 'stream';
 import fs from 'fs';
 import hole, {holeWithArray, holeWithStream} from '../src/main';
 import fetch from 'node-fetch';
 import split2 from 'split2';
 
 describe('Hole', function () {
-  this.timeout(30 * 1000);
+  // this.timeout(30 * 1000);
 
-  it('hole(obj) takes object', async function () { });
-  it('holeWithArray(array) takes array', async function () { });
-  it('holeWithStream(readable) takes readable', async function () { });
-  it('.pipe(fn) transforms data', async function () { });
+  it('hole(obj) takes object', async function () {
+    const expect = {ohh: 'yeah'};
+    let actual;
+    await hole({})
+	.pipe(obj => {
+	  obj.ohh = 'yeah';
+	  return obj;
+	})
+	.pipe(obj => {
+	  actual = obj;
+	});
+    assert.deepStrictEqual(actual, expect);
+  });
+
+  it('holeWithArray(array) takes array', async function () {
+    const expect = ['a', 'b', 'c'];
+    const actual = [];
+    await holeWithArray(['a', 'b', 'c'])
+	.pipe(letter => {
+	  actual.push(letter);
+	});
+    assert.deepStrictEqual(actual, expect);
+  });
+
+  it('holeWithStream(readable) takes readable', async function () {
+    const expect = [0, 1, 2, 3, 4];
+    const actual = [];
+    let i = 0;
+    await holeWithStream(new stream.Readable({
+      read() {
+	this.push(i < 5 ? i : null);
+	i++;
+      },
+      objectMode: true,
+    }))
+	.pipe((i) => {
+	  actual.push(i);
+	});
+    assert.deepStrictEqual(actual, expect);
+  });
+
+  it('.pipe(fn) transforms data', async function () {
+    const expected = [
+      {upper: 'A'},
+      {upper: 'B'},
+      {upper: 'C'},
+      {upper: 'D'},
+      {upper: 'E'},
+    ];
+    const actual = [];
+    await holeWithArray(['a', 'b', 'c', 'd', 'e'])
+	.pipe(letter => letter.toUpperCase())
+	.pipe(async (letter) => {
+	  await timeout(Math.random() * 100);
+	  return letter;
+	})
+	.pipe(upper => actual.push({upper}));
+    assert.deepStrictEqual(actual, expected);
+  });
+
   it('.pipe(async fn) transforms data asyncronously', async function () { });
   it('.pipe(async fn) simaltaniously consumes multiple data', async function () { });
-  it('.pipe(async fn) keeps order', async function () { });
+  it('.pipe(async fn) keeps order', async function () {
+    const expected = ['A', 'B', 'C', 'D', 'E'];
+    const actualUnordered = [];
+    const actualOrdered = [];
+    await holeWithArray(['a', 'b', 'c', 'd', 'e'])
+	.pipe(letter => letter.toUpperCase())
+	.pipe(async (letter) => {
+	  await timeout(Math.random() * 100);
+	  actualUnordered.push(letter);
+	  return letter;
+	})
+	.pipe(upper => {
+	  return actualOrdered.push({upper});
+	});
+    assert.notDeepEqual(actualUnordered, expected);
+    assert.deepStrictEqual(actualOrdered, expected);
+  });
   it('.pipe(transform) accepts native transform', async function () { });
   it('.pipe(transform) accepts third party transform', async function () { });
   it('last .pipe(fn) does not store data in readable buffer even if it returns it', async function () { });
   it('.filter(fn) keeps order', async function () { });
   it('.filter(async fn) keeps order', async function () { });
   it('.piece() splits an passed array', async function () { });
+  it('consumes large number of data', async function () { });
 
   it('accepts object', async function () {
     const url = 'https://jsonplaceholder.typicode.com/posts';
@@ -58,22 +132,6 @@ describe('Hole', function () {
   });
 
   it('holeWithArray accepts array', async function () {
-    const expected = [
-      {upper: 'A'},
-      {upper: 'B'},
-      {upper: 'C'},
-      {upper: 'D'},
-      {upper: 'E'},
-    ];
-    const actual = [];
-    await holeWithArray(['a', 'b', 'c', 'd', 'e'])
-	.pipe(letter => letter.toUpperCase())
-	.pipe(async (letter) => {
-	  await timeout(Math.random() * 100);
-	  return letter;
-	})
-	.pipe(upper => actual.push({upper}));
-    assert.deepStrictEqual(actual, expected);
   });
 
   it('holeWithStream accepts readable stream', async function () {
